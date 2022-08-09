c2cbe41db3a73db1deada5d16db4e2ae5819d1ab
Oguz Sarihan <osarihan@k2m18s03.42kocaeli.com.tr>
2022-08-09 12:06:28 +0300

    a

--- lvl03/add_prime_sum/add_prime_sum.c
+++ lvl03/add_prime_sum/add_prime_sum.c
@@ -6,9 +6,11 @@
 /*   By: osarihan <osarihan@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 00:00:02 by osarihan          #+#    #+#             */
-/*   Updated: 2022/08/09 00:00:02 by osarihan         ###   ########.fr       */
+/*   Updated: 2022/08/09 12:04:52 by osarihan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
 
 int is_prime(int nbr)
 {
@@ -27,7 +29,48 @@ int is_prime(int nbr)
 	return(1);
 }
 
+int ft_atoi(char *av)
+{
+	int res;
+	int total;
+	int i;
+	
+	i = 0;
+	total = 0;
+	res = 1;
+	if(av[i] == '-')
+	{
+		res *= -1;
+		i++;
+	}
+	while (av[i] != '\0')
+	{
+	    total *= 10;
+		total += av[i] - 48;
+		i++;
+	}
+	return(total * res);
+}
+
 int main(int ac, char **av)
 {
-	
+	int i;
+	int count;
+	int nbr;
+	int prm;
+
+	prm = 1;
+	nbr = 0;
+	i = 0;
+	count = 0;
+	nbr = ft_atoi(&av[1][i]);
+	while(nbr >= 0)
+	{
+		if (is_prime(prm) == 1)
+		{
+			count += prm;
+		}
+		prm++;
+	}
+	return (prm);
 }
